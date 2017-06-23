3f7f497c171918dad91ddb48e9132c9e629869fb
Tony Chu <tfchu@Tingfeng-Chus-MacBook-Pro.local>
2017-06-22 23:45:03 -0700

    new

---
+++ duck_game.py
@@ -0,0 +1,10 @@
+'''
+Requirements:
+    1. 4 kinds of ducks - Mallard, Redhead, Rubber, Decoy
+    2. all can swim
+    3. all look different
+    4. fly modes: cannot fly, with wings, ... (more)
+    5. sound: quake, squeak, silent, ... (more)
+    6. during runtime, able to change fly mode and sound
+Apply as many design principals
+'''
